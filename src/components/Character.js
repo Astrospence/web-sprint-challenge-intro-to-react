cab5b6a868ac1036b1bb08215b84fc15d9b252f0
Astrospence <illustrator.wood@gmail.com>
2021-09-10 10:31:07 -0600

    names rendering to page

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props) {
+    const { data } = props;
+
+    return (
+        <div>
+            {data.map(per => {
+                return <div>
+                        <h2>{per.name}</h2>
+                        <button>About</button>
+                        </div>;
+            })}
+        </div>
+    )
+}

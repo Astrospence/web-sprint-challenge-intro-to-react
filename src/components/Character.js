18916bcaab196602bdbf40b4771b5ed69ca63bca
Astrospence <illustrator.wood@gmail.com>
2021-09-10 10:57:54 -0600

    added button listener

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,27 @@
 // Write your Character component here
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Character(props) {
     const { data } = props;
+
+    const [currentCharacter, setCurrentCharacter] = useState(null);
+
+    const openDetails = name => {
+        setCurrentCharacter(name);
+    }
+
+    const closeDetails = () => {
+        setCurrentCharacter(null);
+    }
 
     return (
         <div>
             {data.map(per => {
                 return <div>
                         <h2>{per.name}</h2>
-                        <button>About</button>
-                        </div>;
+                        <button onClick={() => openDetails(per.name)}>About</button>
+                        {currentCharacter ? <p>Info Here</p> : ''}
+                        </div>
             })}
         </div>
     )
